6501d632bccde870fcc307bb52935202b3f71531
SkayLarsen <SkayLarsen@gmail.com>
2016-05-15 14:42:54 +0300

    6

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -с /etc/gunicorn.d/hello hello
+#sudo gunicorn -с /etc/gunicorn.d/hello hello:app
+sudo gunicorn hello:app
