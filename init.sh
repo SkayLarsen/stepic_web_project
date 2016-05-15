f2b8ca79d2f6757407c7bda7c992bab77afcdf3a
SkayLarsen <SkayLarsen@gmail.com>
2016-05-15 16:55:04 +0300

    7

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello /etc/gunicorn.d/hello
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/hello
 sudo /etc/init.d/gunicorn restart
